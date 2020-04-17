a8de677a0d82432650cb79801c8e8dee8a72fe0c
hoangthanhtien <hoangtiendasilva@gmail.com>
2020-04-17 15:55:53 +0700

    refactored our codes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.status(200).json({message: 'Welcome to the project support!'});
-})
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the project support!' });
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
-    console.log(`App is running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`App is running at port ${PORT}`);
+});
